0c9ec0ae635e6462b365164df352e6ac529514d2
Matthew-Likhachev <89824983744@mail.ru>
2026-03-14 12:50:06 +0300

    Изменил вспомогательную программу map_generator_helper.py. Добавил генерацию текста для эксель по всем видам клеток

--- map_generator_helper.py
+++ map_generator_helper.py
@@ -1,15 +1,79 @@
-#полки через одну
-start_id = 99  #номер начальной клетки
-columns_left = 10    #количество рядов полок слева направо
-rows = 9    #количество полок в одном ряду
-step_left = 1   #шаг между колоннами
+def print_res(arr_2d):
+    for el in arr_2d:
+        print(''.join(el))
 
-temp_id = 0
-result = []
-for row in range(rows):
-    line = ['S|---|n-w-s-e|{"shelf_id":'+str(start_id+i)+'}' for i in range (row,columns_left*rows,rows)]
+#полки 1 в ряду
+def f_shelf1(blank,start_id,rows,columns_left,step_left):
+    arr_blank = blank.split('XXX')
+    result = []
+    for row in range(rows):
+        line = [arr_blank[0]+str(start_id+i)+arr_blank[1]+(';' * (step_left + 1)) for i in range (row,columns_left*rows,rows)]
+        line[-1]= line[-1][0:(len(line[-1])-(step_left + 1))] #удаление последних символов ; в последнем элементе
+        result.append(line)
+    return result
+
+#места фасовщика от одного по возрастанию
+def f_Zone_asc(blank, start_id, end_id):
+    arr_blank = blank.split('XXX')
+    result = []
+    line = [arr_blank[0]+str(i)+arr_blank[1]+';' for i in range (start_id, end_id+1)]
+    line[-1] = line[-1][0:(len(line[-1]) - 1)]  # удаление последних символов ; в последнем элементе
     result.append(line)
+    return result
 
-for el in result:
-    empty = ';'*(step_left+1)
-    print(empty.join(el))
+#места фасовщика от 10 по убыванию
+def f_Zone_desc(blank, start_id, end_id):
+    arr_blank = blank.split('XXX')
+    result = []
+    line = [arr_blank[0]+str(i)+arr_blank[1]+';' for i in range (end_id, start_id-1, -1)]
+    line[-1] = line[-1][0:(len(line[-1]) - 1)]  # удаление последних символов ; в последнем элементе
+    result.append(line)
+    return result
+
+#blank = заготовка фразы для клетки. вместо ХХХ подставлять номер порядковый.
+# Формат текста клетки:
+# <Тип>|<Направления_с_грузом>|<Направления_без_груза>|<Логика>
+
+#полки/стеллажи 1 слой в ряду, расстояние между рядами step_left, количество рядов columns_left
+blank = 'S|-w--e|n-w-s-e|{"shelf_id":XXX}'
+start_id = 0  #номер начальной клетки
+rows = 9    #количество полок в одном ряду
+columns_left = 11    #количество рядов полок слева направо
+step_left = 1   #шаг между колоннами
+# print_res(f_shelf1(blank,start_id,rows,columns_left,step_left))
+
+#зона фасовщика по возрастанию
+blank = 'Z|n-w-s-e|---|{"packer_id":2,"zone":"order","slot":XXX}'
+start_id = 1  #номер начальной клетки
+end_id = 10 #номер конечной клетки
+# print_res(f_Zone_asc(blank, start_id, end_id))
+
+#зона фасовщика по убыванию
+blank = 'Z|n-w-s-e|---|{"packer_id":1,"zone":"order","slot":XXX}'
+start_id = 1  #номер начальной (наименьшей по числу) клетки
+end_id = 10 #номер конечной (наибольшей по числу) клетки
+# print_res(f_Zone_desc(blank, start_id, end_id))
+
+#Буферная зона фасовщика по возрастанию
+blank = 'B|n-w-s-e|n-w-s-e|{"packer_id":1,"zone":"buffer","slot":XXX}'
+start_id = 11  #номер начальной клетки
+end_id = 20 #номер конечной клетки
+# print_res(f_Zone_asc(blank, start_id, end_id))
+
+#Буферная зона фасовщика по убыванию
+blank = 'B|n-w-s-e|n-w-s-e|{"packer_id":2,"zone":"buffer","slot":XXX}'
+start_id = 11  #номер начальной клетки
+end_id = 20 #номер конечной клетки
+print_res(f_Zone_desc(blank, start_id, end_id))
+
+#полки/стеллажи 1 слой в ряду, расстояние между рядами step_left, количество рядов columns_left
+blank = 'C|---|n-w-s-e|{"charger_id":XXX}'
+start_id = 1  #номер начальной клетки
+rows = 10    #количество полок в одном ряду
+columns_left = 1    #количество рядов полок слева направо
+step_left = 0   #шаг между колоннами
+# print_res(f_shelf1(blank,start_id,rows,columns_left,step_left))
+
+
+# Свободное пространство (белые клетки)
+# F|n-w-s-e|n-w-s-e|{}
